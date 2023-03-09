c5656ca9437947cfe432158433a0e6dce1da230b
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-09 11:13:19 -0400

    added getimage link route

--- backend/dist/routes/imageRoute.js
+++ backend/dist/routes/imageRoute.js
@@ -4,4 +4,5 @@ const express_1 = require("express");
 const imageController_1 = require("../controllers/imageController");
 const router = (0, express_1.Router)();
 router.post("/", imageController_1.uploadImage);
+router.get("/:imageId", imageController_1.getImageLink);
 exports.default = router;
--- backend/routes/imageRoute.ts
+++ backend/routes/imageRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { uploadImage } from "../controllers/imageController";
+import { getImageLink, uploadImage } from "../controllers/imageController";
 
 const router = Router();
 
 router.post("/", uploadImage);
 
+router.get("/:imageId", getImageLink);
 
 export default router;
