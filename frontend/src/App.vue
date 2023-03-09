abddcdb278e89f1f82c2e23e61ffa4bfee017749
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-09 08:17:58 -0400

    added image form component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <ImageForm />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import ImageForm from './components/ImageForm.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ImageForm
   }
 });
 </script>
