437253e0ad8756674c53c7a407489feeacae2058
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-09 10:07:18 -0400

    refactored

--- backend/dist/routes/imageRoute.js
+++ backend/dist/routes/imageRoute.js
@@ -1,6 +1,8 @@
-import { Router } from "express";
-const router = Router();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
 router.post("/", (_req, res) => {
     res.status(200).json("Image route");
 });
-export default router;
+exports.default = router;
--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,10 +1,16 @@
-import express from "express";
-import cors from "cors";
-import mysql from "mysql2";
-import dotenv from "dotenv";
-import imageRoute from "./routes/imageRoute";
-dotenv.config();
-export const db = mysql.createConnection({
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.db = void 0;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mysql2_1 = __importDefault(require("mysql2"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const imageRoute_1 = __importDefault(require("./routes/imageRoute"));
+dotenv_1.default.config();
+exports.db = mysql2_1.default.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
@@ -12,14 +18,14 @@ export const db = mysql.createConnection({
 });
 class Server {
     constructor() {
-        this.app = express();
+        this.app = (0, express_1.default)();
         this.listen();
     }
     useMiddlewares() {
-        this.app.use(cors());
+        this.app.use((0, cors_1.default)());
     }
     initializeRoutes() {
-        this.app.use("/api/image", imageRoute);
+        this.app.use("/api/image", imageRoute_1.default);
     }
     listen() {
         this.app.listen(process.env.PORT, () => {
