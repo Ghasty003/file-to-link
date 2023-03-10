3766fc2f94d90d3ac83e3c6dc27616a6000bbdf0
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-09 23:56:05 -0400

    cleaned up component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <HomeView />
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HomeView from './views/HomeView.vue'
 
 export default defineComponent({
   components: {
-    HomeView
+   
   },
   setup() {
-    console.log("Hello world")
+    
   },
 })
 </script>
