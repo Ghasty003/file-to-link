9b4c86c536a8796e528f73e504b958fc862f977c
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-09 23:09:29 -0400

    updated

--- backend/controllers/imageController.ts
+++ backend/controllers/imageController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { db } from "../server";
 
-export function uploadImage(req: Request, res: Response) {
-    const { image } = req.body;
-    const q = "INSERT INTO images (`image`) VALUES (?)";
 
-    db.query(q, [image], (err: Error, data: unknown) => {
+export function uploadImage(req: Request, res: Response) {
+    const { image, url } = req.body;
+    const q = "INSERT INTO images (`image`, `url`) VALUES (?)";
+    const values = [image, url]
+   
+    db.query(q, [values], (err: Error, data: unknown) => {
         if (err) res.status(400).json({error: err.message});
 
         res.status(200).json(data);
--- backend/dist/controllers/imageController.js
+++ backend/dist/controllers/imageController.js
@@ -3,9 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getImageLink = exports.uploadImage = void 0;
 const server_1 = require("../server");
 function uploadImage(req, res) {
-    const { image } = req.body;
-    const q = "INSERT INTO images (`image`) VALUES (?)";
-    server_1.db.query(q, [image], (err, data) => {
+    const { image, url } = req.body;
+    const q = "INSERT INTO images (`image`, `url`) VALUES (?)";
+    const values = [image, url];
+    server_1.db.query(q, [values], (err, data) => {
         if (err)
             res.status(400).json({ error: err.message });
         res.status(200).json(data);
