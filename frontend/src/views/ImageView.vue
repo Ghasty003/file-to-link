6ae75862a30d8f04b4023d6e59a50be1ce81da7c
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-10 02:34:16 -0400

    updated

--- frontend/src/views/ImageView.vue
+++ frontend/src/views/ImageView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>{{ $route.params.id }}</h1>
+    <h1>{{ url}}</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  setup() {
+    const url = useRoute().params.id;
+
+    return {
+      url
+    }
+  },
+})
+</script>
