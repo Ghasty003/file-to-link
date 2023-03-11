a8fa068a63b8e9d6cdc6eb8305fb530f3b3775fe
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-11 09:36:22 -0400

    updated nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,13 @@
   <div>
     <nav>
       <h1>Image to Link Converter.</h1>
+      <div>
+        <span>
+          <img src="./assets/image-.gif" alt="">
+          <img src="./assets/export.gif" alt="">
+          <img src="./assets/file.gif" alt="">
+        </span>
+      </div>
     </nav>
     <router-view />
   </div>
@@ -17,5 +24,29 @@ export default defineComponent({
 <style lang="scss">
 body {
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
+
+<style scoped>
+nav {
+  margin-left: 30px;
+}
+
+nav div {
+  width: 300px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+}
+
+span {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 20px;
+}
+
+img {
+  width: 40px;
 }
 </style>
