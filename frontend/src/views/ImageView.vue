1240cbfbfa8c788ed04b50fdd23511671e836c70
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-14 01:20:17 -0300

    refactored

--- frontend/src/views/ImageView.vue
+++ frontend/src/views/ImageView.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="loading" v-if="loading"></p>
     <img v-if="image" :src="imageUrl" alt="image">
-    <embed v-else :src="imageUrl" type="">
+    <iframe v-else :src="imageUrl" />
     <button @click="handleDownload">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -113,7 +113,7 @@ div {
   }
 }
 
-img, embed {
+img, iframe {
   width: 400px;
   height: 400px;
 
